bb4011d014fc68f59de7bb7402da052e88f86c0d
Benjamin Beret <benjamin.beret@gmail.com>
2016-09-08 22:56:57 +0200

    Setup proper testing environment, write first test

---
+++ tests/E7.1.spec.js
@@ -0,0 +1,13 @@
+import {
+  xmlToJS,
+} from '../src/data/index';
+
+import Promise from 'bluebird';
+
+const readFile = Promise.promisify(require('fs').readFile);
+
+test('handle a properly formatted XMAN XML', () => {
+  return readFile('./tests/xman.xml')
+    .then(data => xmlToJS(data))
+    .then(data => expect(data).toMatchSnapshot());
+});
