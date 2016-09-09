659376127fbc61f228cbf2c5f66eeb4b9b5a4f69
Benjamin Beret <benjamin.beret@gmail.com>
2016-09-09 10:43:52 +0200

    Refactor error404 test

--- tests/error404.test.js
+++ tests/error404.test.js
@@ -1,12 +1,6 @@
 import request from 'supertest';
 import app from '../index';
 
-import {readFileSync} from 'fs';
-
-process.env.XMAN_URL = 'http://xmansvr';
-
-const xmanData = readFileSync('./tests/xman.xml', 'utf8');
-
 test('present XML data in JSON format and produce a valid output', () => {
   return request(app)
     .get('/non_existent_url')
