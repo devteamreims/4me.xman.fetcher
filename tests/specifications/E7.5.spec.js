69fab7158eeb52dc69cf2f14c73b3beab8c6023f
Benjamin Beret <benjamin.beret@gmail.com>
2016-09-09 19:26:44 +0200

    Test status value after successful and unsuccessful xman request

--- tests/specifications/E7.5.spec.js
+++ tests/specifications/E7.5.spec.js
@@ -1,6 +1,8 @@
 import request from 'supertest';
-
+import nock from 'nock';
 import app from '../../index';
+
+import {readFileSync} from 'fs';
 
 test('have a /status route', () => {
   return request(app)
@@ -8,5 +10,50 @@ test('have a /status route', () => {
     .expect(200)
     .expect(res => {
       expect(res.body).toMatchSnapshot();
+    });
+});
+
+beforeEach(() => {
+  process.env.XMAN_URL = "http://test-url";
+});
+
+afterEach(() => {
+  delete process.env.XMAN_URL;
+});
+
+test('return proper status after successful request', () => {
+  const xmanRemote = nock(process.env.XMAN_URL)
+    .get('/')
+    .reply(200, readFileSync('./tests/xman.xml'));
+
+  return request(app)
+    .get('/processed')
+    .then(() => {
+      return request(app)
+        .get('/status')
+        .expect(200)
+        .expect(res => {
+          expect(res.body.lastRequest).toBeDefined();
+          expect(res.body.lastRequest.error).toBe(null);
+        });
+    });
+});
+
+test('return proper status after unsuccessful request', () => {
+  const xmanRemote = nock(process.env.XMAN_URL)
+    .get('/')
+    .reply(500);
+
+  return request(app)
+    .get('/processed')
+    .then(() => {
+      return request(app)
+        .get('/status')
+        .expect(200)
+        .expect(res => {
+          console.log(res.body.lastRequest);
+          expect(res.body.lastRequest).toBeDefined();
+          expect(res.body.lastRequest.error).not.toBe(null);
+        });
     });
 });
